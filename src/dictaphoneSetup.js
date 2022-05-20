42d7387ff7a366827054593162563cce59ce1298
Hugo Fang <hugo.fang.25@dartmouth.edu>
2022-05-20 16:04:44 -0400

    New version

--- src/dictaphoneSetup.js
+++ src/dictaphoneSetup.js
@@ -1,9 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 var messageMode; // which type of message to listen to
-var noiseMessage = '', normalMessage = '', secretMessage = ''; // stores most recent versions of each message
 var noiseNormalMessage = '', normalSecretMessage = ''; // combination of two messages
-
+var allMessages = '';
 /*
 TODO: how to merge two lines together
 */
@@ -30,17 +29,12 @@ const Dictaphone1 = () => {
     useEffect(() => {
         if (finalTranscript !== '') {
             console.log('Final transcript:', finalTranscript);
-            if (messageMode === 'noise') {
-                noiseMessage = finalTranscript;
-                console.log("Noise: " + noiseMessage);
+            allMessages = finalTranscript + '\r\n' + allMessages;
+            if (messageMode === 'noiseNormal') {
+                noiseNormalMessage = finalTranscript;
             }
-            else if (messageMode === 'normal') {
-                normalMessage = finalTranscript;
-                console.log("Normal: " + normalMessage);
-            }
-            else if (messageMode === 'secret') {
-                secretMessage = finalTranscript;
-                console.log("Secret: " + secretMessage);
+            else if (messageMode === 'normalSecret') {
+                normalSecretMessage = finalTranscript;
             }
         }
     }, [interimTranscript, finalTranscript]);
@@ -52,19 +46,14 @@ const Dictaphone1 = () => {
         console.log('Your browser does not support speech recognition software! Try Chrome desktop, maybe?');
     }
 
-    const listenForNoise = () => {
-        messageMode = 'noise';
-        noiseMessage = '';
+    const listenForNoiseNormal = () => {
+        messageMode = 'noiseNormal';
+        noiseNormalMessage = '';
         listenContinuously();
     }
-    const listenForNormal = () => {
-        messageMode = 'normal';
-        normalMessage = '';
-        listenContinuously();
-    }
-    const listenForSecret = () => {
-        messageMode = 'secret';
-        secretMessage = '';
+    const listenForNormalSecret = () => {
+        messageMode = 'normalSecret';
+        normalSecretMessage = '';
         listenContinuously();
     }
     const listenContinuously = () => {
@@ -81,23 +70,20 @@ const Dictaphone1 = () => {
 
     const clearAll = () => {
         resetTranscript();
-        noiseMessage = '';
-        normalMessage = '';
-        secretMessage = '';
         noiseNormalMessage = '';
         normalSecretMessage = '';
+        allMessages = '';
     }
     return (
         <div>
             <div>
                 <span>
-                    {listening ? 'I\'m listening!' : 'Click "Noise/Normal/Secret" to start recording'}
+                    {listening ? 'I\'m listening!' : 'Click a message type to start recording'}
                 </span>
             <div>
                 <button type="button" onClick={clearAll}>Clear</button>
-                <button type="button" onClick={listenForNoise}>Noise</button>
-                <button type="button" onClick={listenForNormal}>Normal</button>
-                <button type="button" onClick={listenForSecret}>Secret</button>
+                <button type="button" onClick={listenForNoiseNormal}>Noise + Normal</button>
+                <button type="button" onClick={listenForNormalSecret}>Normal + Secret</button>
                 <button type="button" onClick={stopListening}>Done</button>
             </div>
             </div>
@@ -105,19 +91,7 @@ const Dictaphone1 = () => {
                 {/*<span>{transcript}</span>*/}
             </div>
             <div>
-                Noise message: {noiseMessage}
-            </div>
-            <div>
-                Normal message: {normalMessage}
-            </div>
-            <div>
-                Secret message: {secretMessage}
-            </div>
-            <div>
-                Noise + normal: {noiseNormalMessage}
-            </div>
-            <div>
-                Normal + secret: {normalSecretMessage}
+                {allMessages}
             </div>
         </div>
     );
