2414d0a9385c2dfa4c6b74859162da5603059af0
Hugo Fang <hugo.fang.25@dartmouth.edu>
2022-05-19 22:53:57 -0400

    noise normal secret

--- src/dictaphoneSetup.js
+++ src/dictaphoneSetup.js
@@ -2,6 +2,9 @@ import React, { useEffect, useState } from 'react';
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 
 const Dictaphone1 = () => {
+    var messageMode; // which type of message to listen to
+    var noiseMessage, normalMessage, secretMessage; // stores most recent versions of each message
+
     const [message, setMessage] = useState('');
     const commands = [
         {
@@ -24,6 +27,15 @@ const Dictaphone1 = () => {
     useEffect(() => {
         if (finalTranscript !== '') {
             console.log('Final transcript:', finalTranscript);
+            if (messageMode === 'noise') {
+                noiseMessage = finalTranscript;
+            }
+            else if (messageMode === 'normal') {
+                normalMessage = finalTranscript;
+            }
+            else if (messageMode === 'secret') {
+                secretMessage = finalTranscript;
+            }
         }
     }, [interimTranscript, finalTranscript]);
     if (!SpeechRecognition.browserSupportsSpeechRecognition()) {
@@ -33,29 +45,55 @@ const Dictaphone1 = () => {
     if (!SpeechRecognition.browserSupportsSpeechRecognition()) {
         console.log('Your browser does not support speech recognition software! Try Chrome desktop, maybe?');
     }
+
+    const listenForNoise = () => {
+        messageMode = 'noise';
+        listenContinuously();
+    }
+    const listenForNormal = () => {
+        messageMode = 'normal';
+        listenContinuously();
+    }
+    const listenForSecret = () => {
+        messageMode = 'secret';
+        listenContinuously();
+    }
     const listenContinuously = () => {
         SpeechRecognition.startListening({
             continuous: true,
             language: 'en-GB',
-    });
+        });
     };
+
+    const stopListening = () => {
+        SpeechRecognition.stopListening();
+        resetTranscript();
+    }
     return (
         <div>
             <div>
                 <span>
-                    {listening ? 'I\'m listening!' : 'Press "Listen" to start recording '}
+                    {listening ? 'I\'m listening!' : 'Click "Noise/Normal/Secret" to start recording'}
                 </span>
             <div>
                 <button type="button" onClick={resetTranscript}>Clear</button>
-                <button type="button" onClick={listenContinuously}>Listen</button>
-                <button type="button" onClick={SpeechRecognition.stopListening}>Stop</button>
+                <button type="button" onClick={listenForNoise}>Noise</button>
+                <button type="button" onClick={listenForNormal}>Normal</button>
+                <button type="button" onClick={listenForSecret}>Secret</button>
+                <button type="button" onClick={stopListening}>Done</button>
             </div>
             </div>
             <div>
-                {message}
+                {/*<span>{transcript}</span>*/}
             </div>
             <div>
-                <span>{transcript}</span>
+                Noise message: {noiseMessage}
+            </div>
+            <div>
+                Normal message: {normalMessage}
+            </div>
+            <div>
+                Secret message: {secretMessage}
             </div>
         </div>
     );
